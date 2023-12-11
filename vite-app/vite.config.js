fdc47ea29f7be3426091fc0190f59aa72907a23e
Vignesh Mani <vigneshms565@gmail.com>
2023-12-11 20:02:03 +0800

    update

--- vite-app/vite.config.js
+++ vite-app/vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/perennial_education/perennial_education/web-app/",
+
   plugins: [react()],
+  server: {
+    port: 3000,
+  },
 })
